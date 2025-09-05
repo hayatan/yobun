17bc44d2ed0ea93dd5e0d2875423e9795a1847b0
hayatan <7694503+hayatan@>
2025-09-05 21:56:59 +0900

    スロレポの店舗情報を更新し、ビッグアップル秋葉原店とエスパス上野本館を追加

--- src/config/slorepo-config.js
+++ src/config/slorepo-config.js
@@ -9,12 +9,20 @@ export default {
             code: 'e382a8e382b9e38391e382b9e697a5e68b93e7a78be89189e58e9fe9a785e5898de5ba97code'
         },
         {
-            name: "三ノ輪ＵＮＯ",
-            code: 'e4b889e3838ee8bcaa554e4fcode'
+            name: "ビッグアップル秋葉原店",
+            code: 'e38393e38383e382b0e382a2e38383e38397e383abefbc8ee7a78be89189e58e9fe5ba97code'
         },
         {
             name: "秋葉原UNO",
             code: 'e7a78be89189e58e9f554e4fcode'
+        },
+        {
+            name: "エスパス上野本館",
+            code: 'e382a8e382b9e38391e382b9e697a5e68b93e4b88ae9878ee69cace9a4a8code'
+        },
+        {
+            name: "三ノ輪ＵＮＯ",
+            code: 'e4b889e3838ee8bcaa554e4fcode'
         },
         {
             name: "マルハン新宿東宝ビル店",
@@ -29,20 +37,12 @@ export default {
             code: 'e382b8e383a5e383a9e382afe78e8be5ad90e5ba97code'
         },
         {
-            name: "エスパス上野本館",
-            code: 'e382a8e382b9e38391e382b9e697a5e68b93e4b88ae9878ee69cace9a4a8code'
-        },
-        {
             name: "メッセ竹の塚",
             code: 'e383a1e38383e382bbe7abb9e381aee5a19ae5ba97code'
         },
         {
             name: "ニュークラウン綾瀬店",
             code: 'e3838be383a5e383bce382afe383a9e382a6e383b3e7b6bee780ace5ba97code'
-        },
-        {
-            name: "ビッグアップル秋葉原店",
-            code: 'e38393e38383e382b0e382a2e38383e38397e383abefbc8ee7a78be89189e58e9fe5ba97code'
         },
         {
             name: "タイヨーネオ富山店",
